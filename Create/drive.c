8af85ad9b4e9593d8d4077fe3a73b6ef731b4fc5
Manan Shah <manan.shah.777@gmail.com>
2014-02-14 20:02:02 -0800

    modified drive.c

--- Create/drive.c
+++ Create/drive.c
@@ -1,10 +1,12 @@
 // Created on Fri February 14 2014
 
+#include "./drive.h"
 int main()
 {
 	create_connect();
-	printf("Hello, World!\n");
+	create_wait_time(20); //20 deciseconds
+	create_drive_direct_dist(50); //50 centimeters
+	//block here, square up, and get to the next box area
 	create_disconnect()
 	return 0;
-	//test
 }
