404070827bdbcb461d8888ed3bffcfefa1522f63
Jonas Schill <jonas.schill@gmail.com>
2014-02-16 17:31:13 -0800

    Drives forward approximately the correct distance

--- Create/drive.c
+++ Create/drive.c
@@ -4,9 +4,14 @@
 int main()
 {
 	create_connect();
+	
 	create_wait_time(20); //20 deciseconds
 	create_drive_direct_dist(50); //50 centimeters
 	//block here, square up, and get to the next box area
+	create_block();
+	create_forward(80, 50);
+	//idk what the speeds are so i just put in a random number
+	
 	create_disconnect()
 	return 0;
 }
