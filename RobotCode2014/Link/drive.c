a014a22f200c7bc2fb15716b94307d6d83d32bde
howardwang00 <howardwang00@gmail.com>
2014-03-07 18:30:11 -0800

    included some libraries to link

--- RobotCode2014/Link/drive.c
+++ RobotCode2014/Link/drive.c
@@ -1,13 +1,22 @@
 // Created on Fri February 14 2014
 
 #include ".\drive.h"
+#include "generic.h"
+#include ".\slowservo.h"
 
 int main()
 {
+	light_start()
 	forward(50.00);
 	left(90,0);
 	forward(20.00);
 	right(90,0);
+	//move arm up
+	//move forward so hangers are directly above the scoring pipe
+	//move arm down
+	//collect bottom blue multiplier hangers
+	//go back a little
+	//do same thing as the regular hangers
 	printf("finished");
 	return 0;
 }
