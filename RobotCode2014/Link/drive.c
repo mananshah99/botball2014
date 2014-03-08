27ea8948c3e325addd5aa94744588eecb1af261d
howardwang00 <howardwang00@gmail.com>
2014-03-07 19:17:37 -0800

    Added more link code and updated ks

--- RobotCode2014/Link/drive.c
+++ RobotCode2014/Link/drive.c
@@ -4,19 +4,25 @@
 #include ".\generic.h"
 #include ".\slowservo.h"
 
+#define ARM 3//arm port
+// change "ks" in library
 int main()
 {
-	light_start()
+	light_start();
 	forward(50.00);
 	left(90,0);
 	forward(20.00);
 	right(90,0);
-	//move arm up
-	//move forward so hangers are directly above the scoring pipe
-	//move arm down
-	//collect bottom blue multiplier hangers
+	motor(ARM,50);//move arm up
+	forward(2.5);//move forward so hangers are directly above the scoring pipe
+	motor(ARM,-30);//move arm down to drop hangers
+	left(30,ks/2);
+	right(40,ks/2);//turn to drop hangers down
+	left(10,ks/2);//turn back to be straight against scoring area
+	backward(2.5);//back up
+	motor(ARM,-20);//collect bottom blue multiplier hangers
 	//go back a little
-	//do same thing as the regular hangers
+	//do same thing as the regular hangers to place blue on top
 	printf("finished");
 	return 0;
 }
