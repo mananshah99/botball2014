ded8d96d8b322fc2fd8a7773ee7e6a7f90286d58
Manan Shah <manan.shah.777@gmail.com>
2014-03-07 19:55:47 -0800

    Add forward and backward bump functions

--- RobotCode2014/Create/drive.c
+++ RobotCode2014/Create/drive.c
@@ -1,24 +1,23 @@
 // Created on Fri February 14 2014
-
 #include "./drive.h"
+
+void forward_bump()
+{ while(create_wait_event(LEFT_BUMP) == 0) create_drive_direct(300,300); }
+void backward_bump()
+{ while(create_wait_event(LEFT_BUMP) == 0) create_drive_direct(-300,300); }
+
 int main()
 {
 	create_connect();
 	
-	create_wait_time(20); //20 deciseconds
+	create_wait_time(20); //20 deciseconds for the link
 	create_drive_direct_dist(50); //50 centimeters
-	//block here, square up, and get to the next box area
-
-	while(true){
-		if(//either touch sensor touches
-		){
-			//square up function
-			break;
-		}
-		create_drive_direct(100,1000);
-	}
-	printf("finished");
 	
-	create_disconnect()
+	//block here, square up, and get to the next box area
+	forward_bump();
+	//next box area after squaring up
+	
+	printf("finished");
+	create_disconnect();
 	return 0;
 }
