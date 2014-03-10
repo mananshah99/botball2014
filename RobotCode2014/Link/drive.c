8fbb13b40e8f73db1e7c70bf2d3935d201e0410e
howardwang00 <howardwang00@gmail.com>
2014-03-09 18:49:03 -0700

    added loop to repeat blue hangers one by one

--- RobotCode2014/Link/drive.c
+++ RobotCode2014/Link/drive.c
@@ -20,23 +20,30 @@ int main()
 	left(40,ks/2);
 	right(50,ks/2);
 	left(10,ks/2);//deposit hangers and push them to the sides
-	backward(5.00);//back up
-	motor(ARM,20);
-	forward(5.00);
-	motor(ARM,-20);
-	backward(10.00);
-	motor(ARM,40);
-	forward(10.00);
-	motor(ARM,10);
-	backward(7.00);
-	forward(2.00);
-	motor(ARM,-10);
-	backward(10.00);
-	motor(ARM,10);
-	forward(7.00);//collect one bottom blue multiplier hanger and deliver to top
-	
+	backward(5.00);//back up, going to get blue hangers next
+	while(x<2)
+	{
+		x=1
+		motor(ARM,-50);
+		forward(5.00);//get arm down to the blue hangers
+		motor(ARM,20);
+		forward(5.00);
+		motor(ARM,-20);
+		backward(10.00);
+		motor(ARM,40);
+		forward(10.00);
+		motor(ARM,10);
+		backward(7.00);
+		forward(2.00);
+		motor(ARM,-10);
+		backward(10.00);
+		motor(ARM,10);
+		forward(7.00);//collect one bottom blue multiplier hanger and deliver to top
+		x++
+	{
 	//go back a little
 	//do same thing as the regular hangers to place blue on top
 	printf("finished");
 	return 0;
 }
+
