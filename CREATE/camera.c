aa1a73cbac5fde11146246e720c8b5e9523531f6
Manan Shah <manan.shah.777@gmail.com>
2014-03-14 18:54:57 -0700

    Add create files

---
+++ CREATE/camera.c
@@ -0,0 +1,38 @@
+//general use of the camera
+#define TEST 0
+float _start_time;
+
+void start()
+{
+	_start_time = seconds();
+}
+float curr_time()
+{
+    return seconds()-_start_time;
+}
+void now()
+{
+    printf("now %f\n",curr_time());
+}
+
+void cam_block(){//updates the camera, and waits until success
+	while(!camera_update()) msleep(1);
+}
+
+//self tester
+#ifdef TEST
+int main()
+{
+	int x = 0;
+	camera_open(LOW_RES);
+	cam_block();
+	cam_block();
+	start();
+	while(1)
+	{	
+		cam_block();
+		x++;
+		printf("\n%f",x/curr_time());
+	}
+}
+#endif
