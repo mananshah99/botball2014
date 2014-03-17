2ba9fdaf71f113ee8b26ca926a8ac7a82fff607c
Manan Shah <manan.shah.777@gmail.com>
2014-03-16 18:53:13 -0700

    Minor fixes

--- LINK/servotest.c
+++ LINK/servotest.c
@@ -1,16 +1,13 @@
 // Created on Sun March 16 2014
 
-//#include "./generic.h"
-//#include "./slowservo.h"
-//#include "./drive.h"
-
 #define servo_test
 #ifdef servo_test
 int main()
 {
 	enable_servos();
 	servo_slow(1,1007,5);
-	msleep(1000);
+	msleep(10000);
+	disable_servos();
 	printf("Done!\n");
 	return 0;
 }
