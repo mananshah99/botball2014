f45a7a700b635890cc87a04354a3d4e2cec1480f
howardwang00 <howardwang00@gmail.com>
2014-03-16 18:27:52 -0700

    Added servotest.c

--- LINK/servotest.c
+++ LINK/servotest.c
@@ -1,7 +1,16 @@
 // Created on Sun March 16 2014
 
+#include ./generic.h
+#include ./menu.h
+#include ./slowservo.h
+#include ./drive.h
+#include ./camera.h
+
+#define servo_test
+#ifdef servo_test
 int main()
 {
-	printf("Hello, World!\n");
+	servo_slow(1,1000,5)
+	printf("Done!\n");
 	return 0;
 }
