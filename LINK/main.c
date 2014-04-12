6d6a37e1fa0dbf17d6a7af6015c2922ec32cf722
Howard Wang <howardwang00@gmail.com>
2014-04-11 18:18:49 -0700

    Added DE code and enhanced Seeding code

--- LINK/main.c
+++ LINK/main.c
@@ -14,7 +14,6 @@
 #include "./generic.h"			// generic functionality
 #include "./slowservo.h"		// for slowservo
 
-
 #define ARM 0				// arm port
 #define LS 0				// light sensor port
 #define HANGER_HOLDER 3			// mini-servo for holding hangers in's port
@@ -51,9 +50,13 @@
 #define MIDDLE 1500
 #define RIGHT_CLOSE 0
 
-//Define MAIN to run the primary program
+/** MAIN PROGRAMS **/
+//Define Seeding to run the Seeding program
+//Define DE to run the Double Elimination program
 
-#define MAIN
+//#define MAIN_Seeding
+#define MAIN_DE
+
 //#define servo_test
 //#define DPTEST
 //#define hanger_release_test
@@ -61,11 +64,13 @@
 //#define square_up_and_blue_hanger_test
 
 
-//old start position is with back against PVC pipe and left side 3 inches from left pipe surrounding starting box
-#ifdef MAIN
+/** SEEDING CODE **/
+
+//start position is with back against PVC pipe and left side (wheel) 3 inches from left pipe surrounding starting box
+#ifdef MAIN_Seeding
 int main()	
 {
-	//light_start(LS);			// light start
+	light_start(LS);			// light start
 	shut_down_in(120.);
 	enable_servos();
 	printf("Starting\n");
@@ -98,10 +103,10 @@ int main()
 	bmd(MOT_LEFT);
 	bmd(MOT_RIGHT);
 	*/
-	printf("Lifting up arm....\n");
+	printf("Lifting up arm....!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 	set_servo_position(ARM, TOP);		// move arm up to top ledge
 	msleep(2500);
-	printf("At Hangers and Depositing!\n");
+	printf("At Hangers and Depositing! Chad=Chris!\n");
 	forward(20.00);				// get over to the scoring area
 	msleep(1000);				// stop to stop the arm shaking
 	forward(17.00);
@@ -127,7 +132,7 @@ int main()
 	backward(15.00);
 	msleep(500);
 	
-	printf("Leaving Hangers\n");
+	printf("Leaving hangers/retreating\n");
 	
 	/*  (BACKUP CODE)  wiggle to drop off the hangers 
 	left(40,ks/2);				// left 30 degrees
@@ -135,6 +140,9 @@ int main()
 	left(20,ks/2);				// deposit hangers and push them to the sides, left 20 degrees back
 	*/
 	
+	motor(MOT_RIGHT, 100);
+	msleep(500);
+	ao();
 	motor(MOT_LEFT, -150);
 	msleep(1000);
 	ao();
@@ -143,16 +151,15 @@ int main()
 	msleep(500);
 	set_servo_position(ARM, BOT); //arm will mess up backing up, might will hit rack 
 	msleep(1500);
-	left(270,ks/2);
+	left(220,ks/2);
 	msleep(1500);
 	forward(50.00);
 	right(250,ks/2);				//turn right to square up on pipe next to starting box
 	msleep(500);
 	
 	
-	
-	//THIS PART IS BEING TESTED (blue hanger #1) AND SOON WILL BE REPLACED
-	
+	printf("Blue hanger commencing. Important note: Chad Labuda is weird\n");
+
 	set_servo_position(ARM, BOT);
 	msleep(100);
 	set_servo_position(HANGER_HOLDER, RIGHT_CLOSE);
@@ -162,8 +169,8 @@ int main()
 	msleep(2000);	//square up
 	ao();
 	msleep(500);
-	forward(6.50);				//parallel to rack
-	msleep(500);
+	forward(6.00);				//parallel to rack
+	msleep(10000);
 	right(237,ks/2);
 	msleep(500);
 	ao();			//for next square up
@@ -172,14 +179,21 @@ int main()
 	motor(MOT_RIGHT, -100);		//square up again on other pipe
 	msleep(2000);
 	ao();
-	forward(21.00);
-	msleep(500);
-	forward(68.00);
+	
+	/*
+	forward(19.00);
+	msleep(300);
+	forward(70.00);
 	msleep(500);
 	
 	printf("Now at blue hangers\n");
 	printf("Moving blue hangers to top rack one at a time\n");
 	
+	/*================================================================
+	moving blue hangers up
+	*/
+	
+	/*
 	set_servo_position(ARM, MID);
 	msleep(2000);
 	left(40, ks/2);
@@ -188,7 +202,8 @@ int main()
 	msleep(500);
 	/*right(10, ks/2);
 	msleep(500);*/
-	set_servo_position(ARM, MID-40);
+	/*
+	set_servo_position(ARM, MID-50);
 	msleep(500);
 	backward(10.00);
 	msleep(500);
@@ -206,8 +221,7 @@ int main()
 	msleep(500);
 	motor(MOT_RIGHT, -100);
 	msleep(500);
-	
-	//ENDING OF CODE THAT'S BEING TESTED ELSEWHERE
+	*/
 	
 	
 	/* COMMENTING UNTESTED OLD VERSION OF CODE THAT PROBABLY DON'T WORK, THIS CODE HAS THE ROBOT NOT SQUARING UP
@@ -247,6 +261,214 @@ int main()
 }
 #endif
 
+/** DOUBLE ELIMINATION CODE **/
+
+#ifdef MAIN_DE
+int main()	
+{
+	light_start(LS);			// light start
+	shut_down_in(120.);
+	enable_servos();
+	printf("Starting Program\n");
+	set_servo_position(HANGER_HOLDER, RIGHT_CLOSE);
+	msleep(100);
+	set_servo_position(ARM,BOT);
+	msleep(500);
+	forward(10.00);
+	msleep(500);
+	motor(MOT_LEFT, -50);
+	motor(MOT_RIGHT, -50);
+	msleep(2000);
+	ao();
+	msleep(500);
+	forward(20.00);
+	msleep(500);
+	forward(37.00);
+	msleep(500);
+	left(240,ks/2);				// left 90 degrees (more because the function undershoots)
+	msleep(500);
+	forward(31.00);
+	msleep(500);
+	right(270,ks/2);			// right 90 degrees, but uses the arm to move cube,so adding more, not changed to using both wheels anymore
+	msleep(500);
+	left(20,ks/2); 				//return from pushing exercise bench away from robot
+	backward(15.00);	//sometimes arm hits the bottom rack after
+	msleep(500);
+	/*
+	mrp(MOT_LEFT,400,5);
+	mrp(MOT_RIGHT,-400,5); //turn 90 degrees
+	msleep(1000);
+	bmd(MOT_LEFT);
+	bmd(MOT_RIGHT);
+	*/
+	printf("Lifting up arm....!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+	set_servo_position(ARM, TOP);		// move arm up to top ledge
+	msleep(2500);
+	printf("At Hangers and Depositing! Chad=Chris!\n");
+	forward(20.00);				// get over to the scoring area
+	msleep(1000);				// stop to stop the arm shaking
+	forward(17.00);
+	msleep(500);
+	set_servo_position(HANGER_HOLDER, MIDDLE);	// precaution due to holding on to the left pillar
+	msleep(500);
+	set_servo_position(ARM, DROP);		// move arm down to drop hangers on ledge
+	msleep(500);
+	motor(MOT_LEFT,50);			//going to push it to the right and then turn arm away
+	motor(MOT_RIGHT, -50);			//using both wheels to turn
+	msleep(700);
+	bmd(MOT_LEFT);
+	bmd(MOT_RIGHT);
+	msleep(500);	//stop it running
+	set_servo_position(HANGER_HOLDER, LEFT_CLOSE);
+	msleep(200);
+	ao();	//to stop right motor before I start
+	msleep(500);
+	mrp(MOT_LEFT, 1000, -1500);		//left(220,ks/2); //let go of hangers onto the ledge by turning 90 degrees
+	msleep(1000);
+	ao();
+	msleep(1500);
+	backward(15.00);
+	msleep(500);
+	
+	printf("Leaving hangers/retreating\n");
+	
+	/*  (BACKUP CODE)  wiggle to drop off the hangers 
+	left(40,ks/2);				// left 30 degrees
+	right(60,ks/2);				// right 50 degrees
+	left(20,ks/2);				// deposit hangers and push them to the sides, left 20 degrees back
+	*/
+	
+	motor(MOT_RIGHT, 100);
+	msleep(500);
+	ao();
+	motor(MOT_LEFT, -150);
+	msleep(1000);
+	ao();
+	msleep(500);			//fully let go of hangers, then square up twice on pipes behind
+	forward(10.00);
+	msleep(500);
+	set_servo_position(ARM, BOT); //arm will mess up backing up, might will hit rack 
+	msleep(1500);
+	left(240,ks/2);
+	msleep(1500);
+	forward(50.00);
+	right(250,ks/2);				//turn right to square up on pipe next to starting box
+	msleep(500);
+	
+	
+	printf("Blue hanger commencing. Important note: Chad Labuda is weird\n");
+
+	set_servo_position(ARM, BOT);
+	msleep(100);
+	set_servo_position(HANGER_HOLDER, RIGHT_CLOSE);
+	msleep(300);
+	motor(MOT_LEFT, -100);
+	motor(MOT_RIGHT, -100);
+	msleep(2000);	//square up
+	ao();
+	msleep(500);
+	forward(6.00);				//parallel to rack
+	msleep(10000);
+	right(237,ks/2);
+	msleep(500);
+	ao();			//for next square up
+	msleep(100);
+	motor(MOT_LEFT, -100);
+	motor(MOT_RIGHT, -100);		//square up again on other pipe
+	msleep(2000);
+	ao();
+	
+	/*
+	forward(19.00);
+	msleep(300);
+	forward(70.00);
+	msleep(500);
+	
+	printf("Now at blue hangers\n");
+	printf("Moving blue hangers to top rack one at a time\n");
+	
+	/*================================================================
+	moving blue hangers up
+	*/
+	
+	/*
+	set_servo_position(ARM, MID);
+	msleep(2000);
+	left(40, ks/2);
+	msleep(500);
+	set_servo_position(HANGER_HOLDER, LEFT_CLOSE);
+	msleep(500);
+	/*right(10, ks/2);
+	msleep(500);*/
+	/*
+	set_servo_position(ARM, MID-50);
+	msleep(500);
+	backward(10.00);
+	msleep(500);
+	backward(5.00);
+	msleep(500);
+	set_servo_position(ARM, TOP);
+	msleep(500);
+	forward(15.00);
+	msleep(500);
+	set_servo_position(HANGER_HOLDER, RIGHT_CLOSE);
+	msleep(500);
+	set_servo_position(ARM, DROP);
+	msleep(500);
+	left(10,ks/2);
+	msleep(500);
+	motor(MOT_RIGHT, -100);
+	msleep(500);
+	*/
+	
+	
+	/* COMMENTING UNTESTED OLD VERSION OF CODE THAT PROBABLY DON'T WORK, THIS CODE HAS THE ROBOT NOT SQUARING UP
+	
+	mrp(MOT_LEFT, 500, 1544); 		// undo the turn to be straight again
+	backward(10.00);			// back up 10 cm, going to get blue hangers next
+	msleep(500);
+	
+	
+	/** Don't try FIXING WITH SLOWSERVO anymore, slowservo library broken**/
+	/*
+	int i;			
+	for(i=0; i<2; i++) {			// moves each blue hanger up 
+		set_servo_position(ARM, TOP);	// move to the top 
+		forward(5.00);			// get arm down to the blue hangers
+		set_servo_position(ARM,TOP+20);				
+		forward(5.00);			// check if breaks something
+		set_servo_position(ARM,DROP);
+		backward(10.00);	
+		set_servo_position(ARM,MID);
+		forward(10.00);
+		set_servo_position(ARM,MID+10);
+		backward(7.00);
+		forward(2.00);
+		set_servo_position(ARM,MID);
+		backward(10.00);
+		set_servo_position(ARM,MID+10);
+		forward(7.00);			//collect one bottom blue multiplier hanger and deliver top
+		backward(5.00);
+	}
+	*/
+	
+	
+	ao();
+	printf("finished");
+	return 0;
+}
+#endif
+
+
+
+/**============================================================================**/
+
+
+
+/** WARNING:	TESTING CODES	TESTING CODES	TESTING CODES	TESTING CODES **/
+/** DONT RUN IN TOURNAMENT UNLESS YOU ARE BEING DUMB**/
+
+
 //to test slowservo, it doesn't work currently
 #ifdef servo_test
 int main()
@@ -259,6 +481,7 @@ int main()
 	return 0;
 }
 #endif
+
 
 //Drive Path Test
 #ifdef DPTEST
@@ -277,6 +500,7 @@ int main()
 }
 #endif
 
+
 //Hanger Release Test
 #ifdef hanger_release_test
 int main()
@@ -294,6 +518,7 @@ int main()
 	return 0;
 }
 #endif
+
 
 //left motor test, make sure it starts with the arm up next to top rack holding hangers
 #ifdef left_motor_test
@@ -314,6 +539,9 @@ int main()
 	printf("done\n");
 }
 #endif
+
+
+//TESTING BLUE HANGER # 1, NO TIME SO NOT DOING FOR REGIONALS
 
 #ifdef square_up_and_blue_hanger_test
 
