a4877934dc00e7f5472425c9fadef73ed918e65a
Howard Wang <howardwang00@gmail.com>
2014-06-14 20:54:38 -0700

    got function elevator_lift to work

--- GCER 2014/CREATE/createArm.h
+++ GCER 2014/CREATE/createArm.h
@@ -1,35 +1,32 @@
 // Created on Fri June 13 2014 by Howard Wang
 //Arm libraries: to lift the arm for the Create
 
-#include createDrive.h
-#include singing.h
-#include drive.h
+#include "template.h"
 
-#define front_arm 0				//correct
-#define hanger_holder 1			//
-#define elevator_left_motor 0	//correct
-#define elevator_right_motor 2	//correct
-#define elevator_sensor 0		//
+#define front_arm 0
+#define hanger_holder 3
+#define elevator_left_motor 0
+#define elevator_right_motor 2
+#define elevator_sensor 15
 
-#define front_arm_top_position ?
-#define front_arm_bot_position ?
+#define front_arm_top_position 600
+#define front_arm_bot_position 1900
 
 
 void full_arm_lift()
 {
-	enable_servos();
 	set_servo_position(front_arm ,front_arm_top_position);
-	msleep(300);
 	elevator_lift();
 }
 
 
 void elevator_lift()
 {
-	while(digital(elevator_sensor)==1);
+	printf("%d\n", digital(elevator_sensor));
+	while(digital(elevator_sensor)==0)
 	{
 		motor(elevator_left_motor,100);
-		motor(elevator_right_motor,100);
+		motor(elevator_right_motor,-100);
 		msleep(500);
 	}
 	ao();
@@ -37,7 +34,6 @@ void elevator_lift()
 
 void full_arm_drop()
 {
-	enable_servos();
 	elevator_drop();
 	set_servo_position(front_arm ,front_arm_bot_position);
 	msleep(300);
@@ -45,16 +41,9 @@ void full_arm_drop()
 
 void elevator_drop()
 {
-	while(digital(elevator_sensor)==0);
-	{
-		motor(elevator_left_motor,-50);
-		motor(elevator_right_motor,-50);
-		msleep(500);
-		ao();
-	}
+	motor(elevator_left_motor,-20);
+	motor(elevator_right_motor,-20);
+	msleep(2500);
 	ao();
+	msleep(300);
 }
-
-
-
-
--- GCER 2014/CREATE/main.c
+++ GCER 2014/CREATE/main.c
@@ -1,14 +1,42 @@
 // Created on Sat June 14 2014 by Howard Wang
 
-#include createArm.h
-#include createDrive.h
-#include drive.h
-#include singing.h
-
+#include "./createArm.h"
 //a lot of #defined variables are in "createArm.h"
 
+//#define main
+#define arm_lift
+#define elevator_lift_test
+
+#ifdef main
 int main()
 {
-	printf("Hello, World!\n");
-	return 0;
+	shut_down_in(119.);
+	create_connect();
+	enable_servos();
+	create_drive_right(50,50,90);
+	msleep(100);
+	create_drive_direct_dist(100, 100, 635);
+	msleep(100);
+	create_drive_left(50,50,90);
+	msleep(100);
 }
+#endif
+
+/******************************************************************************************************************************/
+
+#ifdef full_arm_lift
+int main()
+{
+	enable_servos();
+	create_connect();
+	full_arm_lift();
+}
+#endif
+
+#ifdef elevator_lift_test
+int main()
+{
+	elevator_lift();
+}
+#endif
+
