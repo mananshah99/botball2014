3ee501e3bd37c58f3b040681071f76f358284340
Manan Shah <manan.shah.777@gmail.com>
2014-06-15 20:06:28 -0700

    Add #define guard

--- GCER 2014/LINK/main.c
+++ GCER 2014/LINK/main.c
@@ -1,3 +1,4 @@
+#define TEST1
 #ifdef TEST1
 #include "./template.h"
 int main() {
@@ -5,11 +6,11 @@ int main() {
 	camera_open();
 	int x;
 	int y;
-	//while(1) {
+	while(1) {
 
 		printf("area: %d\n",cam_area(0));
 
-		x = -get_object_center(0,0).x;  // get image center x data
+		x = get_object_center(0,0).x;  // get image center x data
 		y = get_object_center(0,0).y;  //    and y data
 		if (get_object_count(0) > 0) { // there is a blob
 			display_printf(0,1,"Center of largest blob: (%d,%d)   ",x,y);
@@ -17,15 +18,15 @@ int main() {
 		else display_printf(0,1,"No object in sight                ");
 		camera_update();   // get new image data before repeating
 
-	//}
+	}
 	
 	//triangle math, much simpler way. test this.
-	int xDif = abs(115-x);
+	/*int xDif = abs(115-x);
 	int yDif = abs(91-y);
 	right(aTan(xDif/yDif)+90);
 	forward(sqrt(xDif*xDif+yDif*yDif));
 
 	set_servo_color(1,0);
-	//118, 9[0-9]
+	//118, 9[0-9]*/
 }
 #endif
