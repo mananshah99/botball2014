82d1bba1fda8f7374975483fe303769d8ea9fd94
Manan Shah <manan.shah.777@gmail.com>
2014-06-17 18:39:40 -0700

    Preliminary pid testing code complete

--- GCER 2014/LINK/pidtest.c
+++ GCER 2014/LINK/pidtest.c
@@ -9,17 +9,19 @@ int main() {
 	//C starts
 	double x = 0.0;
 	while(!c_button()) {
-		WAIT(a_button() || b_button());
+		WAIT(a_button() || b_button() || c_button());
 		if (a_button()) x+=0.5;
 		else if (b_button()) x-=0.5;
+		else break;
 		printf("increasing by -> %f\n", x);
-		WAIT(!(a_button() || b_button()));;
+		WAIT(!(a_button() || b_button() || c_button()));;
 	}
 	printf("x is %f\n", x);
-	K_p += x;
+	K_p = x;
+	printf("K_p is %f\n", K_p);
 	
 	/*
-	rem TURNING: 
+	 TURNING: 
 	
 	while the error is not within a certain EPSILON
 		set the motor power (1) equal to (K_p)*(error)
@@ -36,18 +38,20 @@ int main() {
 	int x_blob, y_blob;
 	int x_rob = 91;
 	int y_rob = -156;
+	camera_update();
 	while(1) {
+		camera_update();
 		printf("area of nearest blob -->  %d\n", cam_area(0));
 		x_blob = get_object_center(0,0).x;  
 		y_blob = get_object_center(0,0).y;  
 		double E = atan(
-			(-1*(x_blob-x_rob))
-			/(y_blob-y_rob)
+			((double)(-1*(x_blob-x_rob)))
+			/((double)(y_blob-y_rob)
 			);
 		motor(MOT_LEFT, K_p* E);
 		motor(MOT_RIGHT, K_p* E);
 		msleep(1);
-		printf("E --> %d,    K_p --> %d\n", E, K_p);
+		printf("E --> %f,    K_p --> %f\n", E, K_p);
 	}
 }
 //#endif
