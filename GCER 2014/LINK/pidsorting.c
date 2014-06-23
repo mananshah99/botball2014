0cab7c785276d6e07b98a23f0aee8b3d90c3b015
Manan Shah <manan.shah.777@gmail.com>
2014-06-22 19:03:41 -0700

    More sorting updates

--- GCER 2014/LINK/pidsorting.c
+++ GCER 2014/LINK/pidsorting.c
@@ -1,19 +1,22 @@
+//#define PID
 #ifdef PID
 #include "./template.h"
 int main() {
 	enable_servos();
 	camera_open();
 	int x, y;
-	printf("area: %d\n", cam_area(0));
-	
+while(1){	
 	x = get_object_center(0,0).x;  
 	y = get_object_center(0,0).y;  
-	
+	/*
 	int x_diff = abs(115-x);
 	int y_diff = abs(91-y);
 	right(atan(x_diff/y_diff)+90);
 	forward(sqrt(x_diff*x_diff+y_diff*y_diff));
-	
+	*/
+	printf("%d    %d\n", x, y);
+	camera_update();
+}
 	/*
 	rem TURNING: 
 	
