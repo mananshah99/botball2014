59d31a1f77fcc29f4d02f6a78bb26fbd7a56dc7d
brandonhuang68 <brandon.huang.68@gmail.com>
2014-06-24 20:55:38 -0700

    Create 2.0.1
    -In position for blue hanger (facing away)
    -now channeling the inner-geese

--- GCER 2014/CreateTwo/main.c
+++ GCER 2014/CreateTwo/main.c
@@ -7,7 +7,9 @@
 #define SENSOR_UP 15
 #define SENSOR_DOWN 14
 #define ARM_UP 150
-#define ARM_DOWN 1900
+#define ARM_UMID 400
+#define ARM_DMID 1200
+#define ARM_DOWN 2000
 #define HANGER_CLOSE 0
 #define HANGER_OPEN 1365
 //#define LIGHTSTART
@@ -16,7 +18,7 @@ void arm_lift(){
 	//arm
 	enable_servo(ARM);
 	set_servo_position(ARM,ARM_UP);
-	msleep(400);
+	msleep(1000);
 	//le elevator
 	while(digital(SENSOR_UP)==0)
 	{
@@ -49,20 +51,38 @@ int main()
 	servo_set(HANGER,HANGER_CLOSE,0.3);
 	sleep(1);
 	
-	create_right(82,0,100); //(angle,radius,speed)
+	create_right(82,0,100); //(angle,radius,speed); 82 = 90deg
 	create_backward(400, 100);// (distance in mm,speed)
-	create_left(85,0,100);
-	create_backward(850, 100);
+	create_left(82,0,100);
+	create_backward(885, 100);
 	create_right(90,0,100);
-	create_backward(300,100);
+	create_backward(250,100);
 	arm_lift();//armlift while robot moves
 	create_block();//At the Pipes
 	
 	servo_set(HANGER,HANGER_OPEN,0.3);
-	create_forward(100,100);
+	create_forward(300,100);
 	create_block();//Backed-up from Pipes
 	
 	arm_lower();
+	create_backward(215,25);
+	create_block();
 	
-	sleep(5);
+	servo_set(HANGER,HANGER_CLOSE,0.5);
+	msleep(500);
+	servo_set(ARM,ARM_DMID,0.5);
+	servo_set(ARM,ARM_DOWN,0.5);
+	create_forward(300,100);
+	create_block();
+	
+	arm_lift();
+	servo_set(ARM,ARM_UMID,0.5);
+	create_backward(215,25);
+	create_block();
+	
+	servo_set(ARM,ARM_UP,0.5);
+	
+	//Temp End Code
+	sleep(1);
+	disable_servos();
 }
