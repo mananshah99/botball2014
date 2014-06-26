6bb7ef64971decfe5badf7d1d9d10fabd1609dc2
osulacam <toby.macaluso@gmail.com>
2014-06-25 19:36:02 -0700

    finished outline of squareup
    I haven't tested it yet but this should square up the robot if it is
    run.  certain constants still need to be tuned or reversed.

--- GCER 2014/LINK/squareUp.c
+++ GCER 2014/LINK/squareUp.c
@@ -1,14 +1,19 @@
 #include "template.h"
 void square_up(int distance){
+	//distance is not in inches do testing to find out for each case distance of 300 is about 3 inches
 	
 	//note don't know if sensor outputs a small number for a big distance or opposite code might have to be
 	//changed because of this
+	//for our purposes it is a small number for a big distance but it is not a linear value so we can't just convert it into inches
 
 	//P constant
 	double Con = 20.0;
 	
+	//motor power for straight part of the squareup
+	int sqpow = 30
+	
 	// distance between IR sensors
-	double dia = 0;
+	double dia = 430;//200units = about 3.5 in they are 7.5 in apart
 	
 	//replace p with the correct values later
 	int leftDistance = analog(int p);
@@ -24,21 +29,31 @@ void square_up(int distance){
 	//difference between the distances
 	int diff = abs(leftDistance-rightDistance);
 	
-	//squares robot
-	while (diff > aDiff) {
-		diff = abs(leftDistance-rightDistance);
-		double AE = atan(diff/dia));
-		//turn robot untill square
-		motor(MOT_LEFT,con*AE));
-		motor(MOT_RIGHT,-1*con*AE);
-	}
+	//allows it to loop so that it can recorect angle and distance 
+	//shouldn't add time if it gets to the right position
+	for (i=0;i<2;i++)
+	{
 	
-	//moves robot to correct distance
-	while (abs((leftDistance+rightDistance)/2-distance) < aDiff){
-		if((leftDistance+rightDistance)/2 < distance){
-			//move robot forward	
-		} else {
-			//move robot backward
+		//squares robot
+		while (diff > aDiff) {
+			diff = abs(leftDistance-rightDistance);
+			double AE = atan(diff/dia));
+			//turn robot until square
+			motor(MOT_LEFT,con*AE));
+			motor(MOT_RIGHT,-1*con*AE);
+		}
+	
+		//moves robot to correct distance
+		while (abs((leftDistance+rightDistance)/2-distance) < aDiff){
+			if((leftDistance+rightDistance)/2 < distance){
+				//move robot forward
+				motor(MOT_LEFT,sqpow));
+				motor(MOT_RIGHT,sqpow);
+			} else {
+				//move robot backward
+				motor(MOT_LEFT,-sqpow));
+				motor(MOT_RIGHT,-sqpow);
+			}
 		}
 	}
 }
