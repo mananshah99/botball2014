21576119baf2cd89363c96e7efa4684b9ba7be4b
Manan Shah <manan.shah.777@gmail.com>
2014-07-01 20:09:05 -0700

    More changes to main.c

--- GCER 2014/LINK/main.c
+++ GCER 2014/LINK/main.c
@@ -1,7 +1,6 @@
 #include "./template.h"
 
 double turned_angle;
-double E = 100;
 int x_rob = 100;  
 int y_rob = -113; //old: 156
 int y_target = 69; //new: 68 (old = 25)
@@ -14,27 +13,33 @@ int y_target = 69; //new: 68 (old = 25)
 int main() {
 	#define DEBUG // comment this out when in actual competition 
 	set_servo_position(3, 100);	
+	
 	//enabling everything
 	enable_servos();
 	camera_open();
 	camera_update();
+	
+	//
 	backward(33);
 	msleep(1000);
 	
+	//
 	set_servo_position(3, 1300);
 	msleep(100);
-	left(195,0);
+	left(175,0);
 	msleep(1000);
 	
+	//
 	backward(6);
 	msleep(1000);
 	
 	int i;
-	for(i=0; i<2; i++){
+	for(i=0; i<3; i++) {
 		correct_angle();
 		correct_distance();
+		msleep(1000);
 	}
-	backward(40);
+	forward(40);
 	//done with backing up 
 	disable_servos();
 }
@@ -42,8 +47,8 @@ int main() {
 void correct_angle() {
 
 //constants
-	double K_p = 23.0;
-	double K_i = 0.05;
+	double K_p = 25.0;
+	double K_i = 0.07;
 	double K_d = 0.01;	
 	
 	//values (rob is robot) 
@@ -122,7 +127,12 @@ void correct_distance() {
 	double E = -y_blob + y_target;
 	
 	//11 used to be 10.4 here
-	backward(-(((double)E)*16)/1000.);
+	if(E < 0) {
+		backward((((double)E)*16)/1000.);
+	}
+	else {	
+		forward((((double)E)*16)/1000.);
+	}
 	msleep(1000);
 	
 	//dropping 
@@ -135,13 +145,19 @@ void correct_distance() {
 	msleep(100);
 	backward(.1);
 	msleep(500);
-	set_servo_position(1, 1800);
+	set_servo_position(1, 1600);
 	msleep(2000);
 	printf("[log] finished tribble pickup");
 	
 	//move back the same amount
-	backward(-(((double)E)*13)/1000.);
+	forward(-(((double)E)*16)/1000.);
+		
 	double angle = turned_angle*RADTODEG;
-	right(angle, 0);
+	if(angle < 0) {
+		left(angle, 0);
+	}
+	else {
+		right(angle, 0);
+	}
 	msleep(1000);
 }
