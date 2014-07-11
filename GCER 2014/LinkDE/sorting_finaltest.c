99ccf7c7418dfc588e54fef23d585d074c4a236a
Manan Shah <manan.shah.777@gmail.com>
2014-07-11 16:42:40 -0700

    Finished tribble sorting
    - angle correction optimized
    - re-added PID for back/forward
    - changed constants for updated ks
    - more minor fixes

--- GCER 2014/LinkDE/sorting_finaltest.c
+++ GCER 2014/LinkDE/sorting_finaltest.c
@@ -1,4 +1,5 @@
-//#define MAIN
+#include "template.h"
+
 #ifndef max
 	#define max( a, b ) ( ((a) > (b)) ? (a) : (b) )
 #endif
@@ -7,10 +8,11 @@
 	#define min( a, b ) ( ((a) < (b)) ? (a) : (b) )
 #endif
 
-#ifdef MAIN
-#include "./template.h"
 
-double turned_angle;
+#define SORTING_FINAL_TEST
+#ifdef SORTING_FINAL_TEST
+
+double turned_angle; 
 int x_rob = 100;  
 int y_rob = -113; //old: 156
 int y_target = 69; //new: 68 (old = 25)
@@ -20,92 +22,11 @@ int y_target = 69; //new: 68 (old = 25)
  * 1300 open
  * port 3
  */
-void correct_angle();
-void correct_distance();
-void square_up_distance(int distance);
-void square_up_angle();
 
 int basket_open = 1300;
 int basket_closed = 171;
 int basket_up = 400;
 int basket_down = 75;
-
-int main() {
-	#define DEBUG // comment this out when in actual competition 
-	/*
-	#define SPDl	90
-	#define SPDr	90
-	#define SPDlb	90
-	#define SPDrb	90
-	#define SPD 	90
-	*/
-	//fixed these in drive.c because stuff
-	
-	set_servo_position(1, 1300);	
-	set_servo_position(2, basket_down);
-	set_servo_position(3, basket_closed);
-	
-	//enabling everything
-	enable_servos();
-	camera_open();
-	camera_update();
-	
-	///---Drive 1---///
-	
-	forward(48);
-	
-	set_servo_position(3, basket_open);
-	
-	msleep(100);
-	left(90, 0);
-	forward(10);
-	printf("sortteding stuffs\n");
-	sleep(3);
-	set_servo_position(3, basket_closed);
-	motor(MOT_LEFT, -60);
-	motor(MOT_RIGHT, -60);
-	sleep(2);
-	servo_slow(2, basket_up, 4);
-	forward(150);
-	set_servo_position(2, basket_down);
-	left(90,0);
-	backward(25);
-	square_up_angle();
-	square_up_distance(240);
-	square_up_angle();
-	msleep(50);
-	right(90,0);
-	backward(10);
-	servo_slow(3, basket_open, 10);
-	sleep(1);
-	forward(25);
-	printf("sortteding stuffs\n");
-	sleep(3);
-	set_servo_position(3, basket_closed);
-	backward(50);
-	right(90,0);
-	
-	
-	//will add this back in later
-	/*
-	#define SPDlb	50
-	#define SPDrb	50
-	#define SPD	20
-	*/
-	/**PICK UP 1*/
-	/*
-	correct_angle();
-	correct_distance();
-	ao();
-	
-	msleep(1500);
-	
-	correct_angle();
-	correct_distance();
-	ao();*/
-	//done with backing up 
-	disable_servos();
-}
 
 void correct_angle() {
 	camera_update();
@@ -129,15 +50,22 @@ void correct_angle() {
 	
 	//init
 	set_servo_position(1, 1584);
+	prev_error = 0;
 	while(1/*!in_range(E, 0, EPSILON) || !in_range(E, 0, -EPSILON)*/) {
-		//x_blob = get_object_center(0,0).x;  
-		//y_blob = get_object_center(0,0).y;  
+		camera_update();
+		x_blob = get_object_center(0,0).x;  
+		y_blob = get_object_center(0,0).y; 
 		do{
 			camera_update();
 			x_blob = get_object_center(0,0).x;  
 			y_blob = get_object_center(0,0).y;  
 		}while(cam_area(0)==0);
-			
+		
+		
+		x_blob = get_object_center(0,0).x;  
+		y_blob = get_object_center(0,0).y;
+		
+		printf("x : %d, y: %d\n");
 		double E = atan(
 			((double)(-1*(x_blob-x_rob)))
 			/((double)(y_blob-y_rob))
@@ -152,11 +80,14 @@ void correct_angle() {
 		integral += (E*0.001); //update time
 		derivative = (E - prev_error)/0.001;
 		
+		if(E*K_p<1 && E*K_p>0) E=1/K_p;
+		if(E*K_p<0 && E*K_p>-1) E=-1/K_p;
+				
 		motor(MOT_LEFT, -1*((K_p*E)+(integral*K_i)+(derivative*K_d)));
 		motor(MOT_RIGHT, (K_p*E)+(integral*K_i)+(derivative*K_d));
 		
 		msleep(1);
-		printf("[log] E -> %f, I -> %f, D -> %f\n", E, integral, derivative);
+		//printf("E -> %f, I -> %f, D -> %f\n", E, integral, derivative);
 		prev_error = E;
 		
 		if(E<=EPSILON && E>=-EPSILON) 
@@ -166,13 +97,104 @@ void correct_angle() {
 		}
 		
 	}
-	printf("[log] done with angle correction");
+	printf("[DONE] done with angle correction");
 	beep();
 	msleep(1000);
 }
 
 void correct_distance() {
+	
+	double K_p = 0.2;
+	double K_i = 0.1;
+	double K_d = 0.001;
+		
+	double x_blob, y_blob;
+	double E = 0;	
+	
+	double integral = 0.0;
+	double derivative = 0.0;
+	double prev_error = 0.0; 
+	
+	double EPSILON = 0.02;
+
+	while(1) {
+		camera_update();
+		
+		printf("area of nearest blob -->  %d\n", cam_area(0));
+		if(cam_area(0)!=0) {
+			x_blob = get_object_center(0,0).x;  
+			y_blob = get_object_center(0,0).y;  
+		}
+		else {
+			printf("!!!!!!!!!! NO BLOB IN SIGHT !!!!!!!!!!!!\n"); 
+			msleep(2000);
+			x_blob = get_object_center(0,0).x;  
+			y_blob = get_object_center(0,0).y;  
+		}
+			
+		E = -y_blob+y_target;
+		
+		//this is a bit sketchy but it should work
+		if(prev_error==0) prev_error = E;
+			
+		integral += (E*0.001); //update time
+		derivative = (E - prev_error)/0.001;
+		
+		int spd = -(K_p*E)+(integral*K_i)+(derivative*K_d);
+		spd = (spd > 60 ? 60 : spd);
+		spd = (spd < -60 ? -60 : spd);
+		
+		motor(MOT_LEFT, spd*3.5);
+		motor(MOT_RIGHT, spd);
+		msleep(1);
+		
+		printf("E -> %f, I -> %f, D -> %f\n", E, integral, derivative);
+		prev_error = E;
+		
+		if(E<=EPSILON && E>=-EPSILON) {
+			ao();
+			break;
+		}
+	}
+	printf("[DONE] done overall correction");
+	msleep(1000);
+	
+	//dropping 
+	servo_slow(1, 200, 5); //port, position, time
+	//shaking
+	forward(.1);
+	msleep(100);
+	backward(.2);
+	msleep(100);
+	forward(.1);
+	msleep(500);
+	set_servo_position(1, 1800);
+	msleep(2000);
+	printf("[DONE] finished tribble pickup");
+	
+	float v = ( ( ( (float) E) * ks )/1000.);
+	//move back the same amount
+	if(v < 0l) 
+		backward(v);
+	else forward(v);
+		
+	float angle = ((float)turned_angle)*RADTODEG;
+	printf("{{ANGLE}} %f\n", angle);
+	printf("   {{TURNED ANGLE}} %f\n", turned_angle);
+	if(angle < 0l) {
+		right(-angle, 0);
+	}
+	else {
+		left(angle, 0);
+	}
+	msleep(1000);
+	turned_angle = 0;
+	
+	
+	
+	/*************************
 	float x_blob, y_blob, E = 0;
+	
 	camera_update();
 	do{
 		x_blob = get_object_center(0,0).x;  
@@ -183,16 +205,15 @@ void correct_distance() {
 	E = y_blob - y_target;
 	
 	//11 used to be 10.4 here
-	float v = ( ( ( (float) E) * ks * 3)/1000.);
-		printf("=============>  %f\n", v);
+	float v = ( ( ( (float) E) * ks )/1000.);
+		printf("v = %f\n", v);
 	if(v < 0l) 
-		forward(v);
-	else {
 		backward(v);
-		printf("forward`~~!!!!!!!");
+	else {
+		forward(v);
 	}
 		
-	printf("[log] done overall");
+	printf("[DONE] done overall correction");
 	msleep(1000);
 	
 	//dropping 
@@ -208,7 +229,7 @@ void correct_distance() {
 	msleep(500);
 	set_servo_position(1, 1800);
 	msleep(2000);
-	printf("[log] finished tribble pickup");
+	printf("[DONE] finished tribble pickup");
 	
 	//move back the same amount
 	if(v < 0l) 
@@ -216,63 +237,18 @@ void correct_distance() {
 	else forward(v);
 		
 	float angle = ((float)turned_angle)*RADTODEG;
-	if(angle < 0) {
-		left(angle, 0);
+	printf("{{ANGLE}} %f\n", angle);
+	printf("   {{TURNED ANGLE}} %f\n", turned_angle);
+	if(angle < 0l) {
+		right(-angle, 0);
 	}
 	else {
-		right(angle, 0);
+		left(angle, 0);
 	}
 	msleep(1000);
+	turned_angle = 0;
+	*******************************/
 }
-
-	/*
-	camera_update();
-	
-	double x_blob, y_blob;
-	
-	//fix this!
-	do{
-		camera_update();
-		x_blob = get_object_center(0,0).x;  
-		y_blob = get_object_center(0,0).y;  
-	}while(cam_area(0)==0);
-	
-	double E = -y_blob + y_target;
-	
-	//11 used to be 10.4 here
-	if(E < 0) {
-		backward((((double)E)*ks)/1000.);
-	}
-	else {	
-		forward((((double)E)*ks)/1000.);
-	}
-	msleep(1000);
-	
-	//dropping 
-	set_servo_position(1, 200);
-	msleep(1500);
-	//shaking
-	backward(.1);
-	msleep(100);
-	forward(.2);
-	msleep(100);
-	backward(.1);
-	msleep(500);
-	set_servo_position(1, 1600);
-	msleep(2000);
-	printf("[log] finished tribble pickup");
-	
-	//move back the same amount
-	forward(-(((double)E)*(ks/2))/1000.);
-		
-	double angle = turned_angle*RADTODEG;
-	if(angle < 0) {
-		left(angle, 0);
-	}
-	else {
-		right(angle, 0);
-	}
-	msleep(1000);*/
 	
 void square_up_angle(){
 	//P constant
@@ -352,4 +328,22 @@ void square_up_distance(int distance){
 			msleep(1);
 		}
 }
+
+int main() {
+	set_servo_position(1, 1300);	
+	set_servo_position(2, basket_down);
+	set_servo_position(3, basket_open);
+	
+	enable_servos();
+	camera_open();
+	camera_update();
+	
+	correct_angle();
+	correct_distance();
+	ao();
+	msleep(1000);
+	correct_angle();
+	correct_distance();
+}
+
 #endif
